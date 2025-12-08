3ed49ebe52f36e221757c442d62ca90ea92e6492
duaa <duamohdali@gmail.com>
2025-12-08 09:31:55 +0530

    Added new module

---
+++ imports.py
@@ -0,0 +1,14 @@
+from math import sqrt , pi
+
+print(sqrt(16))
+print(pi*2)
+
+
+
+import random 
+
+number = random.randint(1,10)
+print(number)
+
+fruits = random.choice(["apple" , "banana" , "kiwi"])
+print(fruits)
