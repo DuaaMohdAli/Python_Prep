9530d94310807ef6d2c26813f28a66f715b71b2a
duaa <duamohdali@gmail.com>
2025-12-12 21:45:38 +0530

    API

---
+++ api.py
@@ -0,0 +1,20 @@
+import requests
+
+# We need coordinates to get weather data
+latitude = 48.85   # Paris latitude
+longitude = 2.35   # Paris longitude
+
+# Build the API URL with our parameters
+url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m"
+
+# Make the request
+response = requests.get(url)
+data = response.json()
+
+data.keys()
+
+type(data["current"])
+
+temperature = data["current"]["temperature_2m"]
+
+print(f"The temperature is {temperature}")
